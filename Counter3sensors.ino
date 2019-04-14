6da0b9266ec9bf73b43c1f69f2d0383cb45fc79c
MRKazemi00 <33034661+MRKazemi00@users.noreply.github.com>
2019-04-14 21:40:17 +0200

    Code for a counter with 3 sensors
    Detta är den slutgiltiga koden man bör använda om man ska ha tre sensorer

---
+++ Counter3sensors.ino
@@ -0,0 +1,26 @@
+int sensorPin1 = 13;
+int sensorPin2 = 12;
+int sensorPin3 = 11;
+int val1;
+int val2;
+int val3;
+int counter = 0;
+
+void setup(){
+  pinMode(sensorPin1,INPUT);
+  pinMode(sensorPin2,INPUT);
+  pinMode(sensorPin3,INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  val1 = digitalRead(sensorPin1);
+  val2 = digitalRead(sensorPin2);
+  val3 = digitalRead(sensorPin3);
+  if((val1 == LOW && val2 == LOW) || (val1 == LOW && val2 == LOW) || (val2 == LOW && val3 == LOW) ){
+    delay(100);
+    counter+=1;
+    Serial.print(counter);
+  }
+
+}
